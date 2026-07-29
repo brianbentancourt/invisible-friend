6fb2c31780057059790f507fb471d1830640c25c
brianbentancourt <brian.bentancourt@sonda.com>
2026-07-29 18:03:11 -0300

    fix: solucionar error de hidratación de NextUI

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body className="dark text-foreground bg-background">
         <Providers>
           <Navbar />
